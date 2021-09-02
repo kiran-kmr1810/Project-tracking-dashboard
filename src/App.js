dc1ad95c190b68180b9c2ff24699acbbc5b28d87
kiran-kmr1810 <kiran.kmr1810@gmail.com>
2021-09-02 17:36:10 +0530

    new

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
   return (
     <div>
       {/*<Student/>*/}
-      <Login/>
+      {/*<Login/>*/}
+      <Mentor/>
     </div>
   );
 }
--- src/Mentor/ProjectList.js
+++ src/Mentor/ProjectList.js
@@ -1,42 +1,53 @@
-
 import "./ProjectList.css";
+import {Stack} from "@chakra-ui/react"
+var data = [
+    {
+    "title" : "Fire Detection using Deep Learning",
+    "field" : "Machine Learning",
+    "group" : "Group - 21",
+    "branch" : "CSE(2021)"
+    },
+    {
+    "title" : "Fire Detection using Deep Learning",
+    "field" : "Machine Learning",
+    "group" : "Group - 21",
+    "branch" : "CSE(2021)"
+    },
+    {
+    "title" : "Fire Detection using Deep Learning",
+    "field" : "Machine Learning",
+    "group" : "Group - 21",
+    "branch" : "CSE(2021)"
+    },
+    {
+    "title" : "Fire Detection using Deep Learning",
+    "field" : "Machine Learning",
+    "group" : "Group - 21",
+    "branch" : "CSE(2021)"
+    },
+];
 
 function ProjectList() {
     return (
         <div className="list">
-            <div className="project-head">Project List</div>
+            <div className="project-head">PROJECT LIST</div>
+            {data.map((info) => {return(
             <div className="project-card">
                 <div className="project-title">
-                    Fire Detection using Deep Learning
+                    {info.title}
                 </div>
                 <div className="project-field">
-                    Machine Learning
+                    {info.field}
                 </div>
                 <div className="project-group">
-                    Group - 21
+                    {info.group}
                 </div>
                 <div className="project-branch">
-                    CSE(2021)
+                    {info.branch}
                 </div>
             
             </div>
-
-            <div className="project-card">
-                <div className="project-title">
-                    Key frame in Video Analytics
-                </div>
-                <div className="project-title-line"/>
-                <div className="project-field">
-                    Evolutionary Algorithm
-                </div>
-                <div className="project-group">
-                    Group - 40
-                </div>
-                <div className="project-branch">
-                    CSE(2021)
-                </div>
-            
-            </div>
+            )})}
     </div>
     )
 }
